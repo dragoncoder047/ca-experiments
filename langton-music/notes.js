bc43caf4a287e741468a0db1d57b51c229d27423
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-08-19 11:13:29 -0400

    add experimental pan support

--- langton-music/notes.js
+++ langton-music/notes.js
@@ -1,19 +1,27 @@
 class Beetle extends Ant {
     constructor(...args) {
         super(...args);
-        this.drum = new Tone.PolySynth(Tone.MembraneSynth).toDestination();
+        this.panner = new Tone.Panner(0).toDestination();
+        this.drum = new Tone.PolySynth(Tone.MembraneSynth).connect(this.panner);
     }
     do_play(arg) {
-        this.drum.triggerAttackRelease(arg, "0.4n");
+        var [_, note, pan] = /([^:]+)(?::([-+\d.]+))?/.exec(arg);
+        if (!pan) pan = 0;
+        this.panner.pan = pan;
+        this.drum.triggerAttackRelease(note, "0.4n");
     }
 }
 
 class Cricket extends Ant {
     constructor(...args) {
         super(...args);
-        this.synth = new Tone.PolySynth(Tone.AMSynth).toDestination();
+        this.panner = new Tone.Panner(0).toDestination();
+        this.synth = new Tone.PolySynth(Tone.AMSynth).connect(this.panner);
     }
     do_play(arg) {
-        this.synth.triggerAttackRelease(arg, "0.4n");
+        var [_, note, pan] = /([^:]+)(?::([-+\d.]+))?/.exec(arg);
+        if (!pan) pan = 0;
+        this.panner.pan = pan;
+        this.synth.triggerAttackRelease(note, "0.4n");
     }
 }
